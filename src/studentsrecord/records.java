2f7583b53be0d5d54d0f719c130ac02bf66f06c7
drexploit01 <ashiqmuhammedep@gmail.com>
2022-07-02 18:27:55 +0000

    first commit

---
+++ src/studentsrecord/records.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package studentsrecord;
+
+/**
+ *
+ * @author SSM LAB
+ */
+public class records {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       login sd = new login();
+                   sd.setVisible(true);
+                   
+    }
+
+    void setVisible(boolean b) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
